6c5e6d4d5b74b3f7440c753fb13e18af7c6ec9ab
Faiqa2022skipq <faiqa.arshad.skipq@gmail.com>
2022-07-14 10:21:35 +0500

    updated api

--- employee-poll/employee-poll/src/Services/API.js
+++ employee-poll/employee-poll/src/Services/API.js
@@ -47,54 +47,7 @@ export default users = {
     }
   }
   
-  let users = {
-    sarahedo: {
-      id: 'sarahedo',
-      password:'password123',
-      name: 'Sarah Edo',
-      avatarURL: "https://www.pexels.com/photo/fashion-woman-girl-cute-41539/",
-      answers: {
-        "8xf0y6ziyjabvozdd253nd": 'optionOne',
-        "6ni6ok3ym7mf1p33lnez": 'optionOne',
-        "am8ehyc8byjqgar0jgpub9": 'optionTwo',
-        "loxhs1bqm25b708cmbf3g": 'optionTwo'
-      },
-      questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
-    },
-    tylermcginnis: {
-      id: 'tylermcginnis',
-      password:'abc321',
-      name: 'Tyler McGinnis',
-      avatarURL: "https://www.pexels.com/photo/man-wearing-white-tank-top-and-sunglasses-taking-close-up-selfie-59576/",
-      answers: {
-        "vthrdm985a262al8qx3do": 'optionOne',
-        "xj352vofupe1dqz9emx13r": 'optionTwo',
-      },
-      questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
-    },
-    mtsamis: {
-      id: 'mtsamis',
-      password:'xyz123',
-      name: 'Mike Tsamis',
-      avatarURL: "https://www.pexels.com/photo/old-man-6110/",
-      answers: {
-        "xj352vofupe1dqz9emx13r": 'optionOne',
-        "vthrdm985a262al8qx3do": 'optionTwo',
-        "6ni6ok3ym7mf1p33lnez": 'optionOne'
-      },
-      questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
-    },
-    zoshikanlu: {
-      id: 'zoshikanlu',
-      password:'pass246',
-      name: 'Zenobia Oshikanlu',
-      avatarURL: "https://www.pexels.com/photo/man-in-gray-formal-suit-jacket-stirring-coffee-during-daytime-111738/",
-      answers: {
-        "xj352vofupe1dqz9emx13r": 'optionOne',
-      },
-      questions: [],
-    }
-  }
+  
   
   let questions = {
     "8xf0y6ziyjabvozdd253nd": {
